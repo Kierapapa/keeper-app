1ebb7e98fe1f1e52517a93bc02bbf7f5da51ec23
Kierapapa <kierapapa@adwebzine.com>
2021-05-14 01:04:32 -0700

    Updates

---
+++ precache-manifest.f7c5282c3227560a94ddec60c73146c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf8e50852b4a5cc4d2b8dad32334cbe",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "65b2f296756a144c21d9",
+    "url": "/keeper-app/static/js/2.9a7cbe45.chunk.js"
+  },
+  {
+    "revision": "703e7f590f288fedde49",
+    "url": "/keeper-app/static/js/main.4732623a.chunk.js"
+  },
+  {
+    "revision": "fb7d45a05bf0dfe0fe92",
+    "url": "/keeper-app/static/js/runtime-main.42e7a143.js"
+  }
+]);
