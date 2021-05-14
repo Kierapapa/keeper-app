04231b32273215d575d7072993c457cfd174465b
Kierapapa <kierapapa@adwebzine.com>
2021-05-14 00:54:20 -0700

    Part 1 Challenge Completed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Keeper-App
 
-React app like Google Keep memo app demo
+React app like Google Keep memo app demo    
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Note = () => {
+    return (
+        <div className="note">
+            <h1>A note title</h1>
+            <p>Content of the note</p>
+        </div>
+    )
+}
+export default Note;
